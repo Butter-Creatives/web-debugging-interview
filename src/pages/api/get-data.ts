cd70abcd1173024c34d28b3e1fb21794b7a6a782
Hans Arijanto <hansarijanto@gmail.com>
2025-05-12 10:46:46 -0400

    break again

--- src/pages/api/get-data.ts
+++ src/pages/api/get-data.ts
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
-  if (req.headers["x-custom-header"] !== "expected-value") {
-    res.status(400).json({ error: "Missing or incorrect custom header" });
-  } else {
-    res.status(200).json({ message: "Counter Ready", timestamp: new Date() });
-  }
+  () => {
+    if (req.headers["x-custom-header"] !== "expected-value") {
+      res.status(400).json({ error: "Missing or incorrect custom header" });
+    } else {
+      res.status(200).json({ message: "Counter Ready", timestamp: new Date() });
+    }
+  };
 }
